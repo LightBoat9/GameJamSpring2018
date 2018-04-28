60b214550815192c7188a2d289ae7c08680de408
Luke <gamersloop@gmail.com>
2018-04-28 14:47:18 -0400

    Drag slot resize

---
+++ Field/Slot/slots_grid.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var slots_amount = Vector2(4,2)
+var offset = Vector2(150, 200)
+
+var Slot = preload("res://Field/Slot/Slot.tscn")
+
+func _ready():
+	for x in slots_amount.x:
+		for y in slots_amount.y:
+			var inst = Slot.instance()
+			add_child(inst)
+			inst.position = Vector2(x,y) * offset
+			
